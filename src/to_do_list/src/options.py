9bef92c6a206e1ffa3d13789145bf9179e0ddefd
Sandeep Karem <sandeepkarem.online@gmail.com>
2025-02-17 18:51:48 +0530

    Update options.py

--- src/to_do_list/src/options.py
+++ src/to_do_list/src/options.py
@@ -4,7 +4,7 @@ from datetime import datetime
 conn = mysql.connector.connect(
     host="localhost",   
     user="root",        
-    password="letmedomywork@77",  
+    password="password",  
     database="todo"  
 )
 
