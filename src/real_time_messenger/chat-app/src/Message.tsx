5f934e8f4a25e5e6eb8af49f0dd7077316a55d9e
Sandeep Karem <sandeepkarem.online@gmail.com>
2025-02-27 19:32:20 +0530

    updated

--- src/real_time_messenger/chat-app/src/Message.tsx
+++
@@ -1,8 +0,0 @@
-function Message(){
-    const name = 'sandeep';
-    if(name)
-        return <h1>hey {name}!</h1>;
-    else
-        return <h1>Hey world!</h1>
-}
-export default Message;
--- src/real_time_messenger/chat_application/msg_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MsgAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'msg_app'
